c64ae909e02378355e4969c51786bc274a2dd210
Shamim <mim24sd@gmail.com>
2022-11-13 18:49:50 +0330

    fix the code to be clear

--- assests/javascript/index.js
+++ assests/javascript/index.js
@@ -4,7 +4,7 @@ const newTask = document.getElementById("task-input");
 const addTaskIcon = document.getElementById("add-task-icon");
 const taskBox = document.getElementById("task-box");
 const newTaskErrorMassage = document.getElementById("title-error");
-const connectionErrorMassage = document.getElementById("connection-error");
+const networkErrorMassage = document.getElementById("network-error");
 
 newTask.addEventListener(
   "keypress",
@@ -35,10 +35,10 @@ async function setNewTask(task) {
       headers,
     });
 
-    const result = await response.json();
+    const {result: { error }} = { result: await response.json() };
 
     if (response.status === 400) {
-      showInputErrorMassage(result.error);
+      showInputErrorMassage(error);
     }
   } catch {
     showConnecionErrorMassage();
@@ -46,35 +46,18 @@ async function setNewTask(task) {
 }
 
 function showConnecionErrorMassage() {
-  connectionErrorMassage.innerHTML = `
-  <p class="connection-error">Something went wrong.</p>
-  <p class="connection-error">Check your connection or try again later.</p>`;
-
-  connectionErrorMassage.classList.remove("connection-error-empty-box");
-  connectionErrorMassage.classList.add("connection-error-full-box");
+  networkErrorMassage.style.visibility = "visible";
 }
 
 function showInputErrorMassage(errorText) {
   newTaskErrorMassage.innerHTML = `<p class="title-error">${errorText}</p>`;
 
-  taskBox.classList.remove("add-task-box");
-  taskBox.classList.add("add-task-box-error");
-
-  newTaskErrorMassage.classList.remove("title-error-empty-box");
-  newTaskErrorMassage.classList.add("title-error-full-box");
+  taskBox.classList.add("error");
 }
 
 function removeErrors() {
   newTaskErrorMassage.innerHTML = "";
+  taskBox.classList.remove("error");
 
-  taskBox.classList.add("add-task-box");
-  taskBox.classList.remove("add-task-box-error");
-
-  newTaskErrorMassage.classList.add("title-error-empty-box");
-  newTaskErrorMassage.classList.remove("title-error-full-box");
-
-  connectionErrorMassage.innerHTML = "";
-
-  connectionErrorMassage.classList.add("connection-error-empty-box");
-  connectionErrorMassage.classList.remove("connection-error-full-box");
+  networkErrorMassage.style.visibility = "hidden";
 }
